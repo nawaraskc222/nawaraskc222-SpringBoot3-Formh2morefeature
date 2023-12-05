1598fecba197c3c0d17e966eb3f631dad2134b18
Nawaras kc <nawaraskc888@gmail.com>
2023-12-05 15:32:29 +0545

    re arrange

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,5 @@
 Added h2 database
 search,display feature added
+
+
+https://youtu.be/UfOxcrxhC0s?si=Z8xMe70mxr3oZOau
--- src/main/java/com/SpringBootDemo5/demo/SpringBootDemo5Application.java
+++ src/main/java/com/SpringBootDemo5/demo/SpringBootDemo5Application.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+
+
+
+
 @SpringBootApplication
 public class SpringBootDemo5Application extends SpringBootServletInitializer{
 
